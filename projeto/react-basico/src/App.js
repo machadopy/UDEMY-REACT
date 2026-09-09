6c053bc61f2cc66b5b510b73fcdb4210c44a90b8
machadopy <vitorandroidcwb@gmail.com>
2026-09-09 20:33:04 -0300

    Refactor Comentario component and update App layout with additional instances

--- projeto/react-basico/src/App.js
+++ projeto/react-basico/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Meu Projecto</h1>
       <h2><Comentario/></h2>
+      <h2><Comentario/></h2>
+      <h2><Comentario/></h2>
     </div>
   );
 }
--- projeto/react-basico/src/components/Comentario.js
+++ projeto/react-basico/src/components/Comentario.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Comentario.css'
 
-const Comentario = ()=> {
-    
-	return <div>Um comentario</div>
-}
+const Comentario = ()=> (
+	<div className="Comentario">
+		<div>Um comentario</div>
+		<div>2 comentario</div>
+	</div>	
+);
 
 export default Comentario;
