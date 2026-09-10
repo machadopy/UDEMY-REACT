20bebdbd9d12927b24d8e3109fc061dd097d3f8a
machadopy <vitorandroidcwb@gmail.com>
2026-09-10 01:15:51 -0300

    Implement form submission for adding new comments in App component

--- projeto/react-basico/src/App.js
+++ projeto/react-basico/src/App.js
@@ -19,26 +19,32 @@ class App extends Component {
         data: new Date(2026,9,9),
         mensagem: '2 comentario',
       }
-    ]
+    ],
+    novoComentario:{
+      nome:'',
+      email:'',
+      mensagem: ''
+    }
   }
 
-  adicionarComentario = () =>{
+  adicionarComentario = (evento) =>{
+    
+    evento.preventDefault();
     console.log("ok")
 
-    const novoComentario = {
-      nome: 'torvi',
-      email: 'torvi@md.com',
-      data: new Date(),
-      mensagem: 'jaz aqui um comentario.'
+    const novoComentario = {...this.state.novoComentario,data: new Date()}
 
-    }
-
-    
 
     this.setState({
-      comentarios: [...this.state.comentarios, novoComentario ]
+      comentarios: [...this.state.comentarios, novoComentario ],
+      novoComentario:{nome:'', email:'', mensagem:''}
   })
 
+  }
+
+  typeValue = evento =>{
+    const {name, value}= evento.target;
+    this.setState({novoComentario:{...this.state.novoComentario, [name]:value}})
   }
 
   render(){
@@ -57,7 +63,23 @@ class App extends Component {
             </Comentario>
 
           )})}
-        <button onClick={this.adicionarComentario}>Adicionar comentario</button>
+        <form method="post" onSubmit={this.adicionarComentario}>
+          <h2>Enviar Comentario</h2>
+
+          <div>
+            <input type="text" name="nome" value={this.state.novoComentario.nome} onChange={this.typeValue} placeholder='Seu nome:' />
+          </div>
+
+          <div>
+            <input type="email" name="email" value={this.state.novoComentario.email}onChange={this.typeValue} placeholder='Seu email:' />
+          </div>
+
+          <div>
+            <textarea name="mensagem" value={this.state.novoComentario.mensagem} onChange={this.typeValue}rows="4"></textarea>
+          </div>
+          <button type="submit">Enviar</button>
+
+        </form>
       </div>
   );
   }
