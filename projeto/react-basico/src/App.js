c2babf9466c4a80be24ca66af68295da0c5d75c8
machadopy <vitorandroidcwb@gmail.com>
2026-09-10 01:35:27 -0300

    Add functionality to remove comments in App component

--- projeto/react-basico/src/App.js
+++ projeto/react-basico/src/App.js
@@ -42,6 +42,14 @@ class App extends Component {
 
   }
 
+  removerComentario = comentario =>{
+    let lista = this.state.comentarios;
+    lista = lista.filter(c=> c !== comentario)
+
+    this.setState({comentarios:lista})
+  }
+
+
   typeValue = evento =>{
     const {name, value}= evento.target;
     this.setState({novoComentario:{...this.state.novoComentario, [name]:value}})
@@ -58,8 +66,10 @@ class App extends Component {
               key={indice}
               nome={comentario.nome}
               email={comentario.email}
-              data={comentario.data}>
+              data={comentario.data}
+              onRemove={this.removerComentario.bind(this, comentario)}>
               {comentario.mensagem}
+              
             </Comentario>
 
           )})}
--- projeto/react-basico/src/components/Comentario.js
+++ projeto/react-basico/src/components/Comentario.js
@@ -9,6 +9,7 @@ const Comentario = (props) => {
 		<p>{props.email}</p>
 		<p>{props.children}</p>
 		<p>{props.data.toString()}</p>
+		<button onClick={props.onRemove}>&times;</button>
 	</div>	
 )};
 
