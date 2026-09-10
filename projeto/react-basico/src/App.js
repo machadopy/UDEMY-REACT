365030b0c0d718cec7297002d77e50f4671dd98c
machadopy <vitorandroidcwb@gmail.com>
2026-09-09 22:05:45 -0300

    Refactor App component to manage state and render Comentario instances dynamically

--- projeto/react-basico/src/App.js
+++ projeto/react-basico/src/App.js
@@ -1,23 +1,49 @@
+import { Component } from 'react';
 import './App.css';
 import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu Projecto</h1>
-          <Comentario nome="joao" email="joao@emial.com" data={new Date(2026,9,9)}>
-            comentario = "Um comentario"
-          </Comentario>
+class App extends Component {
 
-          <Comentario nome="joao" email="joao@emial.com" data={new Date(2026,9,9)}>
-            comentario = "Um comentario"
-          </Comentario>
+  state={
+    comentarios: [
+      {
+        nome: 'Joao',
+        email:'joao@emial.com',
+        data: new Date(2026,9,9),
+        mensagem: 'Um comentario',
+        pk:13
+      },
+      {
+        nome: 'juca',
+        email:'juca@emial.com',
+        data: new Date(2026,9,9),
+        mensagem: '2 comentario',
+        pk:43
+      }
+    ]
+  }
 
-          <Comentario nome="joao" email="joao@emial.com" data={new Date(2026,9,9)}>
-            comentario = "Um comentario"
-          </Comentario>
-    </div>
+  render(){
+    return (
+      <div className="App">
+        <h1>Meu Projecto</h1>
+
+        {this.state.comentarios.map((comentario, indice) =>{
+          return(
+            <Comentario
+              key={comentario.pk}
+              nome={comentario.nome}
+              email={comentario.email}
+              data={comentario.data}>
+              {comentario.mensagem}
+            </Comentario>
+
+          )})}
+
+      </div>
   );
+  }
+  
 }
 
 export default App;
